e673dd898bbeddcc9a0714ce89e889f68c24c249
ramakrishna <ramakrishna.pawar@outlook.com>
2021-07-09 22:18:10 +0530

    Created password validation rule provider.

--- PasswordValidation.Test/Providers/PasswordValidationRuleProviderTest.cs
+++ PasswordValidation.Test/Providers/PasswordValidationRuleProviderTest.cs
@@ -9,13 +9,20 @@ namespace PasswordValidation.Test.Providers
 {
     public class PasswordValidationRuleProviderTest
     {
-        private readonly PasswordValidationRuleProvider _provider;
+        private readonly IPasswordValidationRuleProvider _provider;
         private readonly IMock<PasswordNumberRule> _passwordNumberRule;
+        private readonly IMock<PasswordLengthRule> _passwordLengthRule;
+        private readonly IMock<PasswordLowerCaseRule> _passwordLowerCaseRule;
+        private readonly IMock<PasswordUpperCaseRule> _passwordUpperCaseRule;
 
         public PasswordValidationRuleProviderTest()
         {
             _passwordNumberRule = new Mock<PasswordNumberRule>();
-            _provider = new PasswordValidationRuleProvider(_passwordNumberRule.Object);
+            _passwordLengthRule = new Mock<PasswordLengthRule>();
+            _passwordLowerCaseRule = new Mock<PasswordLowerCaseRule>();
+            _passwordUpperCaseRule = new Mock<PasswordUpperCaseRule>();
+            _provider = new PasswordValidationRuleProvider(_passwordNumberRule.Object, _passwordLengthRule.Object,
+                _passwordLowerCaseRule.Object, _passwordUpperCaseRule.Object);
         }
 
         [Fact]
@@ -24,7 +31,11 @@ namespace PasswordValidation.Test.Providers
             var actual = _provider.ProvideRules();
 
             var rules = actual.Should().BeOfType<List<IRule>>().Subject;
-            rules.Should().HaveCount(1);
+            rules.Should().HaveCount(4);
+            rules[0].Should().BeOfType(_passwordNumberRule.Object.GetType());
+            rules[1].Should().BeOfType(_passwordLengthRule.Object.GetType());
+            rules[2].Should().BeOfType(_passwordLowerCaseRule.Object.GetType());
+            rules[3].Should().BeOfType(_passwordUpperCaseRule.Object.GetType());
         }
     }
 }
--- PasswordValidation/Providers/PasswordValidationRuleProvider.cs
+++ PasswordValidation/Providers/PasswordValidationRuleProvider.cs
@@ -3,18 +3,30 @@ using PasswordValidation.Rules;
 
 namespace PasswordValidation.Providers
 {
-    public class PasswordValidationRuleProvider: IPasswordValidationRuleProvider
+    public class PasswordValidationRuleProvider : IPasswordValidationRuleProvider
     {
         private readonly PasswordNumberRule _passwordNumberRule;
+        private readonly PasswordLengthRule _passwordLengthRule;
+        private readonly PasswordLowerCaseRule _passwordLowerCaseRule;
+        private readonly PasswordUpperCaseRule _passwordUpperCaseRule;
 
-        public PasswordValidationRuleProvider(PasswordNumberRule passwordNumberRule)
+        public PasswordValidationRuleProvider(PasswordNumberRule passwordNumberRule,
+            PasswordLengthRule passwordLengthRule, PasswordLowerCaseRule passwordLowerCaseRule,
+            PasswordUpperCaseRule passwordUpperCaseRule)
         {
             _passwordNumberRule = passwordNumberRule;
+            _passwordLengthRule = passwordLengthRule;
+            _passwordLowerCaseRule = passwordLowerCaseRule;
+            _passwordUpperCaseRule = passwordUpperCaseRule;
         }
 
         public List<IRule> ProvideRules()
         {
-            throw new System.NotImplementedException();
+            return new List<IRule>
+            {
+                _passwordNumberRule, _passwordLengthRule, _passwordLowerCaseRule,
+                _passwordUpperCaseRule
+            };
         }
     }
 }
